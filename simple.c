ef4de0bd14a38edebd8dec5550db33b2769ab576
Douglas De León <douglasdeleon@Douglass-MacBook-Pro.local>
2021-03-28 17:36:33 -0600

    FIX: comillas dobles.

--- simple.c
+++ simple.c
@@ -4,16 +4,16 @@
 #include <linux/list.h>
 
 int simple_init(void) {
-    printk(KERN_INFO “Loading Module\nMensaje Personalizado”);
+    printk(KERN_INFO "Loading Module\nMensaje Personalizado");
     return 0;
 }
 
 void simple_exit(void) {
-    printk(KERN_INFO “Removing Module\nMensaje Personalizado”);
+    printk(KERN_INFO "Removing Module\nMensaje Personalizado");
 }
 
 module_init(simple_init);
 module_exit(simple_exit);
-MODULE_LICENSE(“GPL”);
-MODULE_DESCRIPTION(“DESCRIPCIÓN PERSONALIZADA”);
-MODULE_AUTHOR(“Douglas de León”);
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("DESCRIPCIÓN PERSONALIZADA");
+MODULE_AUTHOR("Douglas de León");
